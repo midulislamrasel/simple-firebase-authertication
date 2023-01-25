0814f2981ee6a19fe7ecec1a8581352a27d4f10b
Midul-islam-Russell <midulraddy121@gmail.com>
2023-01-25 23:08:10 +0600

    add googel and github authorized

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,14 +5,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDc4JCz67xTFcQKEZTx7LJD2Ol1MYY54jE",
-  authDomain: "my-first-project-da167.firebaseapp.com",
-  projectId: "my-first-project-da167",
-  storageBucket: "my-first-project-da167.appspot.com",
-  messagingSenderId: "559920749336",
-  appId: "1:559920749336:web:1324d4a08bfaec6da05c6d"
+  apiKey: "AIzaSyDgB-WVE9Jmf05nWr9EaF17tnLWZV-fI_A",
+  authDomain: "andgithub.firebaseapp.com",
+  projectId: "andgithub",
+  storageBucket: "andgithub.appspot.com",
+  messagingSenderId: "1029501380404",
+  appId: "1:1029501380404:web:11754c676835219a612761"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export default app;
